afbef83f4cea5db15ff45ea0e86ec9c0d1ab07e5
thanerva <bgj170@myy.haaga-helia.fi>
2021-08-03 16:38:09 +0300

    4.3-4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
-    return 1
-}
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, item) => sum + item.likes, 0);
+};
+
+const favouriteBlog = (blogs) => {
+  return blogs.reduce((previous, current) => {
+    return previous.likes > current.likes ? previous : current;
+  });
+};
 
 module.exports = {
-    dummy
-}
+  dummy,
+  totalLikes,
+  favouriteBlog,
+};
